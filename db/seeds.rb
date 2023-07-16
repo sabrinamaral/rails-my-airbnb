ff82d93caef9afb4e53b7109cdaca1400e620e8a
sabrina amaral <sabrinamaraldev@gmail.com>
2023-07-16 17:31:31 -0300

    model, migration and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Flat.destroy_all
+puts 'Deleting old flats from DB'
+puts 'Creating new ones...'
+
+10.times do
+  puts name = Faker::Lorem.sentence
+  puts address = Faker::Address.full_address
+  puts description = Faker::Lorem.paragraph_by_chars
+  puts price_per_night = rand(200..800)
+  puts number_of_guests = rand(1..4)
+
+  flat = Flat.new(name: name, address: address, description: description, price_per_night: price_per_night, number_of_guests: number_of_guests)
+  puts flat.save!
+end
